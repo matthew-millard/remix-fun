1c1c484bccde9b8ac8e6d491a42b919c136dfd49
matthew-millard <123032297+matthew-millard@users.noreply.github.com>
2024-02-13 09:47:52 -0500

    Config(Font): add Soehne-buch as default font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,11 @@ import type { Config } from 'tailwindcss';
 export default {
   content: ['./app/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['soehne-buch', 'ui-sans-serif', 'system-ui'],
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
