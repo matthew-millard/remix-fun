650a4b13548754ea9679253a76be27c060aff66c
matthew-millard <123032297+matthew-millard@users.noreply.github.com>
2024-04-20 15:03:39 -0400

    Feat: implement email verification before allowing the user to create an account

---
+++ app/routes/onboarding.tsx
@@ -0,0 +1,336 @@
+import { getFormProps, getInputProps, useForm } from '@conform-to/react';
+import { parseWithZod } from '@conform-to/zod';
+import { ActionFunctionArgs, json, LoaderFunctionArgs, redirect, type MetaFunction } from '@remix-run/node';
+import { Form, Link, useActionData, useLoaderData } from '@remix-run/react';
+import { AuthenticityTokenInput } from 'remix-utils/csrf/react';
+import { HoneypotInputs } from 'remix-utils/honeypot/react';
+import { z } from 'zod';
+import { AlertToast, ErrorList } from '~/components';
+import { checkCSRF } from '~/utils/csrf.server';
+import { checkHoneypot } from '~/utils/honeypot.server';
+import { requireAnonymous, sessionKey, signup } from '~/utils/auth.server';
+import {
+	EmailSchema,
+	FirstNameSchema,
+	LastNameSchema,
+	PasswordSchema,
+	UsernameSchema,
+} from '~/utils/validation-schemas';
+import { prisma } from '~/utils/db.server';
+import { getSession, sessionStorage } from '~/utils/session.server';
+import { targetQueryParam } from './verify';
+import { verifySessionStorage } from '~/utils/verification.server';
+
+const SignUpSchema = z
+	.object({
+		email: EmailSchema,
+		firstName: FirstNameSchema,
+		lastName: LastNameSchema,
+		username: UsernameSchema,
+		password: PasswordSchema,
+		passwordConfirm: z.string(),
+		rememberMe: z.boolean().optional(),
+	})
+	.refine(data => data.password === data.passwordConfirm, {
+		message: "Passwords don't match",
+		path: ['passwordConfirm'], // This will attach the error to the passwordConfirm field
+	});
+
+export async function loader({ request }: LoaderFunctionArgs) {
+	await requireAnonymous(request);
+
+	const verifySession = await verifySessionStorage.getSession(request.headers.get('Cookie'));
+	const email = verifySession.get(targetQueryParam);
+
+	// This will redirect to the signup page if the email is not verified
+	if (typeof email !== 'string' || !email) {
+		throw redirect('/signup');
+	}
+	const data = {
+		email,
+	};
+
+	return json(data);
+}
+
+export async function action({ request }: ActionFunctionArgs) {
+	await requireAnonymous(request);
+	const formData = await request.formData();
+	await checkCSRF(formData, request.headers);
+	checkHoneypot(formData);
+
+	const submission = await parseWithZod(formData, {
+		schema: SignUpSchema.transform(async (data, ctx) => {
+			// Check if username is already in use
+			const usernameExists = await prisma.username.findFirst({ where: { username: data.username } });
+			if (usernameExists) {
+				ctx.addIssue({ code: z.ZodIssueCode.custom, path: ['username'], message: 'Username is already in use' });
+			}
+
+			return data;
+		}),
+		async: true,
+	});
+
+	if (submission.status !== 'success') {
+		return json(submission.reply({ formErrors: ['Submission failed'] }), {
+			status: submission.status === 'error' ? 400 : 200,
+		});
+	}
+
+	const { email, firstName, lastName, password, username, rememberMe } = submission.value;
+
+	// // Upload users data to db and hash password before storing
+	const session = await signup({ email, firstName, lastName, password, username });
+
+	// Check if user was created
+	if (!session) {
+		return json({ message: 'User could not be created' }, { status: 500 });
+	}
+
+	// Set session cookie
+	const cookieSession = await getSession(request);
+	cookieSession.set(sessionKey, session.id);
+
+	//Redirect to users profile page
+	return redirect(`/${username}/account`, {
+		headers: {
+			'set-cookie': await sessionStorage.commitSession(cookieSession, {
+				expires: rememberMe ? session.expirationDate : undefined,
+			}),
+		},
+	});
+}
+
+export default function SignupRoute() {
+	const { email } = useLoaderData<typeof loader>();
+	const lastResult = useActionData();
+
+	const [form, fields] = useForm({
+		id: 'signup-form',
+		shouldValidate: 'onBlur',
+		lastResult,
+		shouldRevalidate: 'onInput',
+		onValidate({ formData }) {
+			return parseWithZod(formData, { schema: SignUpSchema });
+		},
+		defaultValue: {
+			email,
+		},
+	});
+
+	return (
+		<>
+			<div className="flex flex-1 justify-center overflow-auto">
+				<div className="flex flex-1 flex-col justify-center px-4 py-12 sm:px-6 lg:w-6/12 lg:flex-none lg:px-20 xl:px-24">
+					<div className="mx-auto w-full max-w-sm lg:w-96">
+						<div>
+							<h2 className=" text-2xl font-bold leading-9 tracking-tight text-text-primary">Sign up for an account</h2>
+							<p className="mt-2 text-sm leading-6 text-text-secondary">Become part of our community</p>
+						</div>
+
+						<div className="mt-4">
+							<div>
+								<Form {...getFormProps(form)} method="POST" className="space-y-6">
+									{/* Hidden email input that is prefilled with verified email from cookie */}
+									<input {...getInputProps(fields.email, { type: 'hidden' })} />
+									<div>
+										<label
+											htmlFor={fields.username.id}
+											className="block text-sm font-medium leading-6 text-text-primary"
+										>
+											Username
+										</label>
+										<div className="mt-2">
+											<input
+												{...getInputProps(fields.username, { type: 'text' })}
+												className="block w-full rounded-md border-0 px-2 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+											/>
+											<div
+												className={`transition-height overflow-hidden px-2 py-1 duration-500 ease-in-out ${fields.username.errors ? 'max-h-56' : 'max-h-0'}`}
+											>
+												<ErrorList errors={fields.username.errors} id={fields.username.errorId} />
+											</div>
+										</div>
+									</div>
+									<div>
+										<label
+											htmlFor={fields.firstName.id}
+											className="block text-sm font-medium leading-6 text-text-primary"
+										>
+											First name
+										</label>
+										<div className="mt-2">
+											<input
+												{...getInputProps(fields.firstName, { type: 'text' })}
+												className="block w-full rounded-md border-0 px-2 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+											/>
+											<div
+												className={`transition-height overflow-hidden px-2 py-1 duration-500 ease-in-out ${fields.firstName.errors ? 'max-h-56' : 'max-h-0'}`}
+											>
+												<ErrorList errors={fields.firstName.errors} id={fields.firstName.errorId} />
+											</div>
+										</div>
+									</div>
+
+									<div>
+										<label
+											htmlFor={fields.lastName.id}
+											className="block text-sm font-medium leading-6 text-text-primary"
+										>
+											Last name
+										</label>
+										<div className="mt-2">
+											<input
+												{...getInputProps(fields.lastName, { type: 'text' })}
+												className="block w-full rounded-md border-0 px-2 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+											/>
+											<div
+												className={`transition-height overflow-hidden px-2 py-1 duration-500 ease-in-out ${fields.lastName.errors ? 'max-h-56' : 'max-h-0'}`}
+											>
+												<ErrorList errors={fields.lastName.errors} id={fields.lastName.errorId} />
+											</div>
+										</div>
+									</div>
+
+									<div>
+										<label
+											htmlFor={fields.password.id}
+											className="block text-sm font-medium leading-6 text-text-primary"
+										>
+											Password
+										</label>
+										<div className="mt-2">
+											<input
+												{...getInputProps(fields.password, { type: 'password' })}
+												className="block w-full rounded-md border-0 px-2 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+											/>
+											<div
+												className={`transition-height overflow-hidden px-2 py-1 duration-500 ease-in-out ${fields.password.errors ? 'max-h-56' : 'max-h-0'}`}
+											>
+												<ErrorList errors={fields.password.errors} id={fields.password.errorId} />
+											</div>
+										</div>
+									</div>
+
+									<div>
+										<label
+											htmlFor={fields.passwordConfirm.id}
+											className="block text-sm font-medium leading-6 text-text-primary"
+										>
+											Password Confirm
+										</label>
+										<div className="mt-2">
+											<input
+												{...getInputProps(fields.passwordConfirm, { type: 'password' })}
+												className="block w-full rounded-md border-0 px-2 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+											/>
+											<div
+												className={`transition-height overflow-hidden px-2 py-1 duration-500 ease-in-out ${fields.passwordConfirm.errors ? 'max-h-56' : 'max-h-0'}`}
+											>
+												<ErrorList errors={fields.passwordConfirm.errors} id={fields.passwordConfirm.errorId} />
+											</div>
+										</div>
+									</div>
+									<div>
+										<HoneypotInputs />
+										<AuthenticityTokenInput />
+									</div>
+									<div className="flex items-center justify-between">
+										<div className="flex items-center">
+											<input
+												id="remember-me"
+												name="remember-me"
+												type="checkbox"
+												className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
+											/>
+											<label htmlFor="remember-me" className="ml-3 block text-sm leading-6 text-text-primary">
+												Remember me
+											</label>
+										</div>
+
+										<div className="text-sm leading-6">
+											<Link to="/login" className="font-semibold text-indigo-600 hover:text-indigo-500">
+												Already have an account?
+											</Link>
+										</div>
+									</div>
+
+									<div>
+										<button
+											type="submit"
+											className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+										>
+											Sign up
+										</button>
+									</div>
+									<div
+										className={`transition-height overflow-hidden px-2 py-1 duration-500 ease-in-out ${form.errors ? 'max-h-56' : 'max-h-0'}`}
+									>
+										<AlertToast errors={form.errors} id={form.errorId} />
+									</div>
+								</Form>
+							</div>
+
+							<div className="mt-10">
+								<div className="relative">
+									<div className="absolute inset-0 flex items-center" aria-hidden="true">
+										<div className="w-full border-t border-gray-200" />
+									</div>
+									<div className="relative flex justify-center text-sm font-medium leading-6">
+										<span className="bg-bg-primary px-6 text-text-primary">Or continue with</span>
+									</div>
+								</div>
+
+								<div className="mt-6 grid grid-cols-2 gap-4">
+									<a
+										href="/"
+										className="flex w-full items-center justify-center gap-3 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus-visible:ring-transparent"
+									>
+										<svg className="h-5 w-5" aria-hidden="true" viewBox="0 0 24 24">
+											<path
+												d="M12.0003 4.75C13.7703 4.75 15.3553 5.36002 16.6053 6.54998L20.0303 3.125C17.9502 1.19 15.2353 0 12.0003 0C7.31028 0 3.25527 2.69 1.28027 6.60998L5.27028 9.70498C6.21525 6.86002 8.87028 4.75 12.0003 4.75Z"
+												fill="#EA4335"
+											/>
+											<path
+												d="M23.49 12.275C23.49 11.49 23.415 10.73 23.3 10H12V14.51H18.47C18.18 15.99 17.34 17.25 16.08 18.1L19.945 21.1C22.2 19.01 23.49 15.92 23.49 12.275Z"
+												fill="#4285F4"
+											/>
+											<path
+												d="M5.26498 14.2949C5.02498 13.5699 4.88501 12.7999 4.88501 11.9999C4.88501 11.1999 5.01998 10.4299 5.26498 9.7049L1.275 6.60986C0.46 8.22986 0 10.0599 0 11.9999C0 13.9399 0.46 15.7699 1.28 17.3899L5.26498 14.2949Z"
+												fill="#FBBC05"
+											/>
+											<path
+												d="M12.0004 24.0001C15.2404 24.0001 17.9654 22.935 19.9454 21.095L16.0804 18.095C15.0054 18.82 13.6204 19.245 12.0004 19.245C8.8704 19.245 6.21537 17.135 5.2654 14.29L1.27539 17.385C3.25539 21.31 7.3104 24.0001 12.0004 24.0001Z"
+												fill="#34A853"
+											/>
+										</svg>
+										<span className="text-sm font-semibold leading-6">Google</span>
+									</a>
+
+									<a
+										href="/"
+										className="flex w-full items-center justify-center gap-3 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus-visible:ring-transparent"
+									>
+										<svg className="h-5 w-5 fill-[#24292F]" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
+											<path
+												fillRule="evenodd"
+												d="M10 0C4.477 0 0 4.484 0 10.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0110 4.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.203 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.942.359.31.678.921.678 1.856 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0020 10.017C20 4.484 15.522 0 10 0z"
+												clipRule="evenodd"
+											/>
+										</svg>
+										<span className="text-sm font-semibold leading-6">GitHub</span>
+									</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
+}
+
+export const meta: MetaFunction = () => {
+	return [{ title: 'BarFly | Sign up' }, { name: 'description', content: 'Insert page description here!' }];
+};
--- app/routes/verify.tsx
+++ app/routes/verify.tsx
@@ -1,7 +1,192 @@
-export default function Verify() {
+import { getFormProps, getInputProps, useForm } from '@conform-to/react';
+import { getZodConstraint, parseWithZod } from '@conform-to/zod';
+import { verifyTOTP } from '@epic-web/totp';
+import { ActionFunctionArgs, json, LoaderFunctionArgs, redirect } from '@remix-run/node';
+import { Form, useActionData, useSearchParams } from '@remix-run/react';
+import { AuthenticityTokenInput } from 'remix-utils/csrf/react';
+import { z } from 'zod';
+import { AlertToast, ErrorList } from '~/components';
+import { checkCSRF } from '~/utils/csrf.server';
+import { prisma } from '~/utils/db.server';
+import { verifySessionStorage } from '~/utils/verification.server';
+
+export const codeQueryParam = 'code';
+export const typeQueryParam = 'type';
+export const targetQueryParam = 'target';
+export const redirectToQueryParam = 'redirectTo';
+
+const types = ['signup'] as const;
+const verificationTypeSchema = z.enum(types);
+
+const VerifySchema = z.object({
+	[codeQueryParam]: z.string().min(6).max(6),
+	[typeQueryParam]: verificationTypeSchema,
+	[targetQueryParam]: z.string(),
+	[redirectToQueryParam]: z.string().optional(),
+});
+
+export async function loader({ request }: LoaderFunctionArgs) {
+	const params = new URL(request.url).searchParams;
+
+	if (!params.has(codeQueryParam)) {
+		return json({
+			status: 'idle',
+			submission: {
+				intent: '',
+				payload: Object.fromEntries(params) as Record<string, unknown>,
+				error: {} as Record<string, Array<string>>,
+			},
+		} as const);
+	}
+
+	return validateRequest(request, params);
+}
+
+export async function action({ request }: ActionFunctionArgs) {
+	const formData = await request.formData();
+	checkCSRF(formData, request.headers);
+	return validateRequest(request, formData);
+}
+
+async function validateRequest(request: Request, body: URLSearchParams | FormData) {
+	const submission = await parseWithZod(body, {
+		schema: VerifySchema.transform(async (data, ctx) => {
+			const verification = await prisma.verification.findUnique({
+				where: {
+					target_type: {
+						target: data[targetQueryParam],
+						type: data[typeQueryParam],
+					},
+					OR: [{ expiresAt: { gt: new Date() } }, { expiresAt: null }],
+				},
+				select: {
+					secret: true,
+					period: true,
+					digits: true,
+					charSet: true,
+					algorithm: true,
+				},
+			});
+
+			if (!verification) {
+				ctx.addIssue({
+					path: [codeQueryParam],
+					code: z.ZodIssueCode.custom,
+					message: `Invalid code`,
+				});
+				return z.NEVER;
+			}
+
+			const codeIsValid = verifyTOTP({
+				otp: data[codeQueryParam],
+				...verification,
+			});
+			if (!codeIsValid) {
+				ctx.addIssue({
+					path: [codeQueryParam],
+					code: z.ZodIssueCode.custom,
+					message: `Invalid code`,
+				});
+				return z.NEVER;
+			}
+
+			return data;
+		}),
+
+		async: true,
+	});
+
+	if (submission.status !== 'success') {
+		return json(submission.reply({ formErrors: ['Submission failed'] }), {
+			status: submission.status === 'error' ? 400 : 200,
+		});
+	}
+
+	const { target, type } = submission.value;
+
+	await prisma.verification.delete({
+		where: {
+			target_type: { target, type },
+		},
+	});
+
+	const verifySession = await verifySessionStorage.getSession(request.headers.get('cookie'));
+	verifySession.set(targetQueryParam, target);
+
+	return redirect('/onboarding', {
+		headers: {
+			'set-cookie': await verifySessionStorage.commitSession(verifySession),
+		},
+	});
+}
+
+export default function VerifyRoute() {
+	const lastResult = useActionData();
+	const [searchParams] = useSearchParams();
+
+	const [form, fields] = useForm({
+		id: 'verify-form',
+		constraint: getZodConstraint(VerifySchema),
+		shouldValidate: 'onBlur',
+		lastResult,
+		shouldRevalidate: 'onInput',
+		defaultValue: {
+			[codeQueryParam]: searchParams.get(codeQueryParam) ?? '',
+			[typeQueryParam]: searchParams.get(typeQueryParam) ?? '',
+			[targetQueryParam]: searchParams.get(targetQueryParam) ?? '',
+			[redirectToQueryParam]: searchParams.get(redirectToQueryParam) ?? '',
+		},
+		onValidate({ formData }) {
+			return parseWithZod(formData, { schema: VerifySchema });
+		},
+	});
 	return (
-		<div>
-			<h1>Verify</h1>
+		<div className=" flex h-full flex-col justify-center p-6">
+			<div className="mx-auto max-w-2xl rounded-lg bg-bg-alt shadow-lg">
+				<div className="p-6">
+					<h3 className="text-base font-semibold leading-6 text-text-primary">Please verify your account</h3>
+					<div className="mt-2 max-w-xl text-sm text-text-secondary">
+						<p>We&apos;ve sent you a code to verify your email address</p>
+					</div>
+					<Form {...getFormProps(form)} method="POST" className="mt-5 sm:flex sm:items-center">
+						<AuthenticityTokenInput />
+						<div className=" flex w-full flex-col sm:max-w-xs">
+							<div className="flex flex-col sm:flex-row ">
+								<label htmlFor={fields[codeQueryParam].id} className="sr-only">
+									One-Time Password
+								</label>
+								<input
+									{...getInputProps(fields[codeQueryParam], { type: 'text' })}
+									className="block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+									placeholder="Please enter your 6 digit code"
+									// eslint-disable-next-line jsx-a11y/no-autofocus
+									autoFocus
+								/>
+								<input {...getInputProps(fields[typeQueryParam], { type: 'hidden' })} />
+								<input {...getInputProps(fields[targetQueryParam], { type: 'hidden' })} />
+								<input {...getInputProps(fields[redirectToQueryParam], { type: 'hidden' })} />
+
+								<button
+									type="submit"
+									className="mt-3 inline-flex w-full items-center justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 sm:ml-3 sm:mt-0 sm:w-auto"
+								>
+									Submit
+								</button>
+							</div>
+							<div
+								className={`transition-height overflow-hidden  py-1 duration-500 ease-in-out ${fields[codeQueryParam].errors ? 'max-h-56' : 'max-h-0'}`}
+							>
+								<ErrorList errors={fields[codeQueryParam].errors} id={fields[codeQueryParam].errorId} />
+							</div>
+						</div>
+					</Form>
+					<div
+						className={`transition-height overflow-hidden  py-1 duration-500 ease-in-out ${form.errors ? 'max-h-56' : 'max-h-0'}`}
+					>
+						<AlertToast errors={form.errors} id={form.errorId} />
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 }
