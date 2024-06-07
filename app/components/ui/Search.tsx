671f9f96de7f06c9883702748c82f27b432e17c0
matthew-millard <123032297+matthew-millard@users.noreply.github.com>
2024-06-07 13:38:19 -0400

    feat(search): implement search functionality, users can search barflies

--- app/components/ui/Search.tsx
+++ app/components/ui/Search.tsx
@@ -5,69 +5,98 @@ import {
 	ComboboxOptions,
 	Dialog,
 	DialogPanel,
+	Label,
 	Transition,
 	TransitionChild,
 } from '@headlessui/react';
 import { ChevronRightIcon, MagnifyingGlassIcon } from '@heroicons/react/20/solid';
 import { UsersIcon } from '@heroicons/react/24/outline';
-import { useState } from 'react';
+import { Form, Link, useLoaderData, useRouteLoaderData, useSearchParams, useSubmit } from '@remix-run/react';
+import { useEffect, useId, useState } from 'react';
+import { useDebounce } from '~/hooks/useDebounce';
+import { loader } from '~/root';
+import ImageChooser from '../ImageChooser';
 
 function classNames(...classes: string[]) {
 	return classes.filter(Boolean).join(' ');
 }
 
-interface Person {
+interface User {
 	id: string;
-	name: string;
-	phone: string;
 	email: string;
-	role: string;
-	url: string;
-	profileUrl: string;
-	imageUrl: string;
+	firstName: string;
+	lastName: string;
+	username: string;
+	profileImageAltText: string | null;
+	profileImageContentType: string | null;
+	profileImageBlob: Buffer | null;
+	userLocation: string;
+	createdAt: Date;
 }
 
-const people: Person[] = [
-	{
-		id: '1',
-		name: 'Finn Crooks',
-		phone: '1-613-223-9371',
-		email: 'finncrooks@barfly.ca',
-		role: 'Co-Founder / CEO',
-		url: 'https://barfly.ca/finncrooks',
-		profileUrl: '#',
-		imageUrl:
-			'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-	},
-	{
-		id: '2',
-		name: 'Hamish Millard',
-		phone: '1-493-747-9031',
-		email: 'lesliealexander@example.com',
-		role: 'Co-Founder / CEO',
-		url: 'https://example.com',
-		profileUrl: '#',
-		imageUrl:
-			'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-	},
-];
+// 	{
+// 		id: '1',
+// 		name: 'Finn Crooks',
+// 		phone: '1-613-223-9371',
+// 		email: 'finncrooks@barfly.ca',
+// 		role: 'Co-Founder / CEO',
+// 		url: 'https://barfly.ca/finncrooks',
+// 		profileUrl: '#',
+// 		imageUrl:
+// 			'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+// 	},
+// 	{
+// 		id: '2',
+// 		name: 'Hamish Millard',
+// 		phone: '1-493-747-9031',
+// 		email: 'lesliealexander@example.com',
+// 		role: 'Co-Founder / CEO',
+// 		url: 'https://example.com',
+// 		profileUrl: '#',
+// 		imageUrl:
+// 			'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+// 	},
+// ];
 
-const recent: Person[] = [people[0], people[1]];
+// const recent: Person[] = [people[0], people[1]];
 
 interface SearchProps {
 	isOpen: boolean;
 	closeSearch: () => void;
+	status?: 'idle' | 'loading' | 'success' | 'error';
+	autoFocus?: boolean;
+	autoSubmit?: boolean;
 }
 
-export default function Search({ isOpen, closeSearch }: SearchProps) {
-	const [query, setQuery] = useState('');
+export default function Search({ isOpen, closeSearch, status, autoFocus = false, autoSubmit = false }: SearchProps) {
+	const { filteredUsers = [] } = useRouteLoaderData('routes/search+/index') || {};
+	console.log('filteredUsers: ', filteredUsers);
 
-	const filteredPeople: Person[] =
-		query === ''
-			? []
-			: people.filter(person => {
-					return person.name.toLowerCase().includes(query.toLowerCase());
-				});
+	const [query, setQuery] = useState('');
+	const [searchParams] = useSearchParams();
+	const id = useId();
+	const submit = useSubmit();
+
+	const handleFormChange = useDebounce((form: HTMLFormElement) => {
+		submit(form);
+	}, 400);
+
+	const [profileImageUrls, setProfileImageUrls] = useState<Record<string, string | null>>({});
+
+	useEffect(() => {
+		if (filteredUsers.length > 0) {
+			const urls: Record<string, string | null> = {};
+			filteredUsers.forEach(user => {
+				if (user.profileImageId) {
+					urls[user.id] = `/resources/images/${user.profileImageId}/profile`;
+				} else {
+					urls[user.id] = null;
+				}
+			});
+			setProfileImageUrls(urls);
+		}
+	}, [filteredUsers]);
+
 	return (
 		<Transition show={isOpen} afterLeave={() => setQuery('')} appear>
 			<Dialog className="relative z-10" onClose={closeSearch}>
@@ -82,7 +111,12 @@ export default function Search({ isOpen, closeSearch }: SearchProps) {
 					<div className="fixed inset-0 bg-slate-900 bg-opacity-50 backdrop-blur-sm" />
 				</TransitionChild>
 
-				<div className="fixed inset-0 z-10 w-screen overflow-y-auto p-4 sm:p-6 md:p-20">
+				<Form
+					method="GET"
+					action="/search"
+					className="fixed inset-0 z-10 w-screen overflow-y-auto p-4 sm:p-6 md:p-20"
+					onChange={e => autoSubmit && handleFormChange(e.currentTarget)}
+				>
 					<TransitionChild
 						enter="ease-out duration-300"
 						enterFrom="opacity-0 scale-95"
@@ -92,7 +126,7 @@ export default function Search({ isOpen, closeSearch }: SearchProps) {
 						leaveTo="opacity-0 scale-95"
 					>
 						<DialogPanel className="mx-auto max-w-3xl transform divide-y divide-border-tertiary overflow-hidden rounded-md bg-bg-secondary shadow-2xl ring-1 ring-inset ring-border-tertiary transition-all  ">
-							<Combobox<Person> onChange={(person: Person) => (window.location.href = person.profileUrl)}>
+							<Combobox<User>>
 								{({ activeOption }) => (
 									<>
 										<div className="relative">
@@ -100,16 +134,21 @@ export default function Search({ isOpen, closeSearch }: SearchProps) {
 												className="pointer-events-none absolute left-4 top-3.5 h-5 w-5 text-gray-400"
 												aria-hidden="true"
 											/>
+											<Label htmlFor={id} className="sr-only"></Label>
 											<ComboboxInput
-												autoFocus
+												type="search"
+												name="query"
+												id={id}
+												defaultValue={searchParams.get('query') ?? ''}
+												autoFocus={autoFocus}
 												className=" h-12 w-full rounded-t-md border-0  bg-transparent pl-11 pr-4 text-text-primary placeholder:text-gray-400 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500 focus:ring-offset-0 sm:text-sm"
-												placeholder="Barflies, bars..."
+												placeholder="Barflies, bars, cocktails..."
 												onChange={event => setQuery(event.target.value)}
 												onBlur={() => setQuery('')}
 											/>
 										</div>
 
-										{(query === '' || filteredPeople.length > 0) && (
+										{(query === '' || !filteredUsers || filteredUsers.length > 0) && (
 											<ComboboxOptions
 												as="div"
 												static
@@ -126,11 +165,11 @@ export default function Search({ isOpen, closeSearch }: SearchProps) {
 														<h2 className="mb-4 mt-2 text-xs font-semibold text-text-secondary">Recent searches</h2>
 													)}
 													<div className="-mx-2 text-sm text-text-secondary">
-														{(query === '' ? recent : filteredPeople).map(person => (
+														{(query === '' ? filteredUsers : filteredUsers).map(user => (
 															<ComboboxOption
 																as="div"
-																key={person.id}
-																value={person}
+																key={user.id}
+																value={user}
 																className={({ focus }) =>
 																	classNames(
 																		'flex cursor-default select-none items-center rounded-md p-2',
@@ -140,8 +179,18 @@ export default function Search({ isOpen, closeSearch }: SearchProps) {
 															>
 																{({ focus }) => (
 																	<>
-																		<img src={person.imageUrl} alt="" className="h-6 w-6 flex-none rounded-full" />
-																		<span className="ml-3 flex-auto truncate">{person.name}</span>
+																		<span className="flex h-8 w-8 overflow-hidden rounded-full bg-gray-200">
+																			{profileImageUrls[user.id] ? (
+																				<img
+																					src={profileImageUrls[user.id]}
+																					alt={user.profileImageAltText || 'Profile'}
+																					className="h-full w-full object-cover"
+																				/>
+																			) : (
+																				<UsersIcon className="h-full w-full text-gray-400" aria-hidden="true" />
+																			)}
+																		</span>
+																		<span className="ml-3 flex-auto truncate">{`${user.firstName} ${user.lastName}`}</span>
 																		{focus && (
 																			<ChevronRightIcon
 																				className="ml-3 h-5 w-5 flex-none text-gray-400"
@@ -157,41 +206,49 @@ export default function Search({ isOpen, closeSearch }: SearchProps) {
 
 												{activeOption && (
 													<div className="hidden h-96 w-1/2 flex-none flex-col divide-y divide-border-tertiary overflow-y-auto sm:flex">
-														<div className="flex-none p-6 text-center">
-															<img src={activeOption.imageUrl} alt="" className="mx-auto h-16 w-16 rounded-full" />
-															<h2 className="mt-3 font-semibold text-text-primary">{activeOption.name}</h2>
-															<p className="text-sm leading-6 text-gray-500">{activeOption.role}</p>
+														<div className="flex flex-col items-center p-6 text-center">
+															<span className="flex h-20 w-20 overflow-hidden rounded-full bg-gray-200 ">
+																{profileImageUrls[activeOption.id] ? (
+																	<img
+																		src={profileImageUrls[activeOption.id]}
+																		alt={activeOption.profileImageAltText || 'Profile'}
+																		className="h-full w-full object-cover"
+																	/>
+																) : (
+																	<UsersIcon className="h-full w-full text-gray-400" aria-hidden="true" />
+																)}
+															</span>
+															<h2 className="mt-3 font-semibold text-text-primary">
+																{activeOption.firstName} {activeOption.lastName}
+															</h2>
+															<p className="text-sm leading-6 text-gray-500">{activeOption.username}</p>
 														</div>
 														<div className="flex flex-auto flex-col justify-between p-6">
-															<dl className="grid grid-cols-1 gap-x-6 gap-y-3 text-sm text-gray-700">
+															<dl className="grid grid-cols-1 gap-x-6 gap-y-3 text-sm text-text-secondary">
 																<dt className="col-end-1 font-semibold text-text-primary">Location</dt>
-																<dd>{activeOption.phone}</dd>
-																<dt className="col-end-1 font-semibold text-text-primary">URL</dt>
-																<dd className="truncate">
-																	<a href={activeOption.url} className="text-indigo-600 underline">
-																		{activeOption.url}
-																	</a>
-																</dd>
-																<dt className="col-end-1 font-semibold text-text-primary">Email</dt>
-																<dd className="truncate">
-																	<a href={`mailto:${activeOption.email}`} className="text-indigo-600 underline">
-																		{activeOption.email}
-																	</a>
+																<dd>{activeOption.userLocation}</dd>
+																<dt className="col-end-1 font-semibold text-text-primary">Member since</dt>
+																{/* calculate the date the user registered */}
+
+																<dd>
+																	{new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'long' }).format(
+																		new Date(activeOption.createdAt),
+																	)}
 																</dd>
 															</dl>
-															<button
-																type="button"
-																className="mt-6 w-full rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+															<Link
+																to={`/${activeOption.username}/profile`}
+																className="mt-6 flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
 															>
 																View profile
-															</button>
+															</Link>
 														</div>
 													</div>
 												)}
 											</ComboboxOptions>
 										)}
 
-										{query !== '' && filteredPeople.length === 0 && (
+										{query !== '' && filteredUsers.length === 0 && (
 											<div className="px-6 py-14 text-center text-sm sm:px-14">
 												<UsersIcon className="mx-auto h-6 w-6 text-gray-400" aria-hidden="true" />
 												<p className="mt-4 font-semibold text-text-primary">No people found</p>
@@ -205,7 +262,7 @@ export default function Search({ isOpen, closeSearch }: SearchProps) {
 							</Combobox>
 						</DialogPanel>
 					</TransitionChild>
-				</div>
+				</Form>
 			</Dialog>
 		</Transition>
 	);
---
+++ app/routes/search+/index.tsx
@@ -0,0 +1,61 @@
+import { json, LoaderFunctionArgs, redirect } from '@remix-run/node';
+import { profile } from 'console';
+import { z } from 'zod';
+import { prisma } from '~/utils/db.server';
+
+const UserSchema = z.object({
+	id: z.string(),
+	email: z.string(),
+	firstName: z.string(),
+	lastName: z.string(),
+	username: z.string(),
+	profileImageId: z.string().nullable(),
+	userLocation: z.string(),
+	createdAt: z.date(),
+});
+
+const UsersSchema = z.array(UserSchema);
+
+export async function loader({ request }: LoaderFunctionArgs) {
+	const query = new URL(request.url).searchParams.get('query');
+
+	if (query === '') {
+		return redirect('/', { status: 302 });
+	}
+
+	const like = `%${query}%`;
+
+	const rawUsers = await prisma.$queryRaw`
+	SELECT 
+		U.id, 
+		U.email, 
+		U.firstName, 
+		U.lastName, 
+		UN.username AS username,
+		UPI.id AS profileImageId,
+		UL.city || ', ' || UL.country AS userLocation,
+		U.createdAt
+	FROM User U
+	LEFT JOIN UserProfileImage UPI ON U.id = UPI.userId
+	LEFT JOIN UserLocation UL ON U.id = UL.userId
+	LEFT JOIN Username UN ON U.id = UN.userId
+	WHERE 
+		U.email LIKE ${like} OR 
+		U.firstName LIKE ${like} OR 
+		U.lastName LIKE ${like}`;
+
+	const result = UsersSchema.safeParse(rawUsers);
+
+	console.log('result', result);
+
+	if (!result.success) {
+		return json({
+			message: "We couldn't find anything with that term. Please try again.",
+		} as const);
+	}
+
+	return json({
+		status: 'idle',
+		filteredUsers: result.data,
+	} as const);
+}
