0bda37af71a85d4bb1d4839bed8ec24f7ca5a97a
matthew-millard <123032297+matthew-millard@users.noreply.github.com>
2024-06-12 14:16:39 -0400

    Feat(search): update rawQuery to order by first and last name

--- app/utils/searchByQuery.ts
+++ app/utils/searchByQuery.ts
@@ -26,7 +26,17 @@ export async function searchUsersByQuery(query: string): Promise<{ filteredUsers
 		UN.username AS username,
 		UPI.id AS profileImageId,
 		UL.city || ', ' || UL.country AS userLocation,
-		U.createdAt
+		U.createdAt,
+	CASE
+		WHEN U.firstName LIKE ${like} THEN 1
+		WHEN U.lastName LIKE ${like} THEN 2
+		WHEN U.email LIKE ${like} THEN 3
+		WHEN UN.username LIKE ${like} THEN 4
+		WHEN UL.city LIKE ${like} THEN 5
+		WHEN UL.province LIKE ${like} THEN 6
+		WHEN UL.country LIKE ${like} THEN 7
+		ELSE 8
+	END AS relevance
 	FROM User U
 	LEFT JOIN UserProfileImage UPI ON U.id = UPI.userId
 	LEFT JOIN UserLocation UL ON U.id = UL.userId
@@ -39,8 +49,8 @@ export async function searchUsersByQuery(query: string): Promise<{ filteredUsers
 		UL.city LIKE ${like} OR
 		UL.province LIKE ${like} OR
 		UL.country LIKE ${like}
-	LIMIT 50
-		`;
+	ORDER BY relevance, U.firstName, U.lastName
+	LIMIT 20`;
 
 	const result = UsersSchema.safeParse(rawUsers);
 
