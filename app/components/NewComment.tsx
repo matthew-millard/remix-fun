8be48f28b6498286d4ba057f06a5c757c1c53159
matthew-millard <123032297+matthew-millard@users.noreply.github.com>
2024-07-03 14:37:26 -0400

    feat(reviews): add styling to new comment and reviews components

--- app/components/NewComment.tsx
+++ app/components/NewComment.tsx
@@ -1,35 +1,35 @@
 export default function NewComment() {
 	return (
-		<div className="mt-16 flex flex-col gap-y-2">
-			<div className="flex gap-x-2">
-				<img
-					src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-					alt=""
-					className="h-6 w-6 flex-none rounded-full bg-gray-50"
-				/>
-				<form action="#" className="flex-auto" id="comment-form">
-					<div className="overflow-hidden rounded-lg pb-12 shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-indigo-600">
-						<label htmlFor="comment" className="sr-only">
-							Add your comment
-						</label>
-						<textarea
-							rows={1}
-							name="comment"
-							id="comment"
-							className="block w-full resize-none border-0 bg-transparent px-2 py-1.5 text-text-primary placeholder:text-text-secondary focus:ring-0 sm:text-sm sm:leading-6"
-							placeholder="Add your comment..."
-							defaultValue={''}
-						/>
-					</div>
-				</form>
-			</div>
-			<button
-				form="comment-form"
-				type="submit"
-				className="max-w-24 self-end rounded-md bg-indigo-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white dark:bg-white dark:text-indigo-600 dark:hover:bg-indigo-50"
-			>
-				Comment
-			</button>
+		<div className="mt-6 flex gap-x-3">
+			<img
+				src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+				alt=""
+				className="hidden h-5 w-5 flex-none rounded-full bg-gray-50 lg:block lg:h-6 lg:w-6"
+			/>
+			<form action="#" className="relative flex-auto">
+				<div className="overflow-hidden rounded-lg pb-12 shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-indigo-600">
+					<label htmlFor="comment" className="sr-only">
+						Add your comment
+					</label>
+					<textarea
+						rows={2}
+						name="comment"
+						id="comment"
+						className="block w-full resize-none border-0 bg-transparent py-2 pl-3 pr-2 text-sm text-text-primary placeholder:text-sm placeholder:text-gray-400 focus:outline-none sm:leading-6 lg:text-base lg:placeholder:text-base"
+						placeholder="Add your comment..."
+						defaultValue={''}
+					/>
+				</div>
+
+				<div className="absolute  bottom-0 right-0 flex py-2 pl-3 pr-2">
+					<button
+						type="submit"
+						className="shadow-lsm items-end rounded-md bg-indigo-500 px-2.5 py-1.5 text-sm  font-semibold text-white ring-1 ring-inset ring-bg-primary  hover:bg-indigo-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white dark:bg-white dark:text-indigo-600 dark:hover:bg-gray-300"
+					>
+						Comment
+					</button>
+				</div>
+			</form>
 		</div>
 	);
 }
--- app/components/ProTip.tsx
+++ app/components/ProTip.tsx
@@ -3,7 +3,7 @@ import { Cocktail } from '~/routes/cocktails+/$cocktail';
 
 export default function ProTip({ cocktail }: { cocktail: Cocktail }) {
 	return (
-		<fieldset className="mt-3 flex flex-col  rounded-md border border-border-primary p-4 pb-2 shadow-sm">
+		<fieldset className="mt-3 flex flex-col  rounded-md border border-border-secondary p-4 pb-2 shadow-sm">
 			<legend className="flex items-center gap-x-2 pl-1 pr-2">
 				<BoltIcon className="h-4 w-4 text-yellow-400 lg:h-5 lg:w-5" />
 				<h3 className="text-sm font-semibold text-text-primary lg:text-base">
@@ -13,7 +13,7 @@ export default function ProTip({ cocktail }: { cocktail: Cocktail }) {
 			<div className="-mt-2">
 				{cocktail.proTips.map((proTip, index) => {
 					return (
-						<ul key={index} className="mb-2 flex items-baseline gap-x-2 text-sm text-text-secondary lg:text-base">
+						<ul key={index} className="mb-2 flex items-baseline gap-x-2 text-sm text-text-primary lg:text-base">
 							{cocktail.proTips.length > 1 ? (
 								<span className="text-text-primary" aria-hidden="true">
 									{index + 1}.
